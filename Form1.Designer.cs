593ceab32407a12c2b28050477c8996c14478d4f
Administrator <Administrator@DEVELOP-001>
2022-11-08 12:14:15 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace netugazou_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.名前をつけて保存SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.バージョン情報AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(426, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くOToolStripMenuItem,
+            this.名前をつけて保存SToolStripMenuItem,
+            this.バージョン情報AToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // 開くOToolStripMenuItem
+            // 
+            this.開くOToolStripMenuItem.Name = "開くOToolStripMenuItem";
+            this.開くOToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.開くOToolStripMenuItem.Text = "開く(&O)";
+            this.開くOToolStripMenuItem.Click += new System.EventHandler(this.開くOToolStripMenuItem_Click);
+            // 
+            // 名前をつけて保存SToolStripMenuItem
+            // 
+            this.名前をつけて保存SToolStripMenuItem.Name = "名前をつけて保存SToolStripMenuItem";
+            this.名前をつけて保存SToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.名前をつけて保存SToolStripMenuItem.Text = "名前をつけて保存(&S)";
+            this.名前をつけて保存SToolStripMenuItem.Click += new System.EventHandler(this.名前をつけて保存SToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Location = new System.Drawing.Point(130, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(69, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CPU温度 ：";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Location = new System.Drawing.Point(51, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 320);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "最大温度：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "最小温度：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox2.Location = new System.Drawing.Point(301, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(69, 19);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox3.Location = new System.Drawing.Point(301, 67);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(69, 19);
+            this.textBox3.TabIndex = 7;
+            // 
+            // バージョン情報AToolStripMenuItem
+            // 
+            this.バージョン情報AToolStripMenuItem.Name = "バージョン情報AToolStripMenuItem";
+            this.バージョン情報AToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.バージョン情報AToolStripMenuItem.Text = "バージョン情報(&A)";
+            this.バージョン情報AToolStripMenuItem.Click += new System.EventHandler(this.バージョン情報AToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 469);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "netugazou";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くOToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ToolStripMenuItem 名前をつけて保存SToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem バージョン情報AToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace netugazou_v1
+{
+    public partial class Form1 : Form
+    {
+        static string fileName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 開くOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "CSV ファイル (*.csv)|*.csv|すべてのファイル (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.Cancel) return;
+
+            fileName = ofd.FileName;
+            //MessageBox.Show(fileName, "お知らせ"); //debug
+
+            var list = loadCSV(fileName);
+            //MessageBox.Show(list[1][0], "CPU温度");
+            // list[0][0] = "TEMP"
+            // list[1][0] = thermo_of_CPU
+            textBox1.Text = list[1][0]; // set_CPU_Thermo_to_textbox1
+
+            //var maxmax = list_Max(list);
+            //var minmin = list_Min(list);
+
+            //MessageBox.Show(maxmax.ToString(),"最大");
+            //MessageBox.Show(minmin.ToString(), "最小");
+
+            DrawThermoImage(list);
+
+
+        }
+        public void ColorScaleBCGYR(double input, ref int r, ref int g, ref int b)
+        {
+            //thermo color gen
+            double tmp_input = Math.Cos(4 * Math.PI * input);
+            int col_val = (int)((-tmp_input / 2 + 0.5) * 255);
+            if (input >= (4.0 / 4.0)) { r = 255; g = 0; b = 0; }
+            else if (input >= (3.0 / 4.0)) { r = 255; g = col_val; b = 0; }
+            else if (input >= (2.0 / 4.0)) { r = col_val; g = 255; b = 0; }
+            else if (input >= (1.0 / 4.0)) { r = 0; g = 255; b = col_val; }
+            else if (input >= (0.0 / 4.0)) { r = 0; g = col_val; b = 255; }
+            else { r = 0; g = 0; b = 255; }
+        }
+        public double list_Max(string[][] nums) //return_max_of_list
+        {
+            if (nums.Length == 0) return 0;
+            double max = double.Parse(nums[2][0]);
+            for (int i = 2; i < nums.Length; i++)
+            {
+                max = max > double.Parse(nums[i][0]) ? max : double.Parse(nums[i][0]);
+                // Minの場合は不等号を逆にすればOK
+            }
+            return max;
+        }
+        public double list_Min(string[][] nums)//return_min_of_list
+        {
+            if (nums.Length == 0) return 0;
+            double min = double.Parse(nums[2][0]);
+            for (int i = 2; i < nums.Length; i++)
+            {
+                min = min < double.Parse(nums[i][0]) ? min : double.Parse(nums[i][0]);
+                // Minの場合は不等号を逆にすればOK
+            }
+            return min;
+        }
+        private string[][] loadCSV(string filePath)// get_information_from_*.CSV
+        {
+            var list = new List<string[]>();
+            StreamReader reader =
+            new StreamReader(filePath, System.Text.Encoding.GetEncoding("Shift_JIS"));
+            while (reader.Peek() >= 0)
+            {
+                list.Add(reader.ReadLine().Split(','));
+            }
+            reader.Close();
+            return list.ToArray();
+        }
+        private void DrawThermoImage(string[][] list)// draw_Thermograph_to_picbox1
+        {
+            //Graphics objGrp = pictureBox1.CreateGraphics();
+            pictureBox1.Image = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            Graphics objGrp = Graphics.FromImage(pictureBox1.Image);
+
+            //objGrp.DrawLine(objPen, 20, 20, 200, 200);
+
+            var maxmax = list_Max(list); textBox2.Text = maxmax.ToString();
+            var minmin = list_Min(list); textBox3.Text = minmin.ToString();
+
+
+            for (int i = 2; i < list.Length; i++)
+            {
+                double thermo_color = (double.Parse(list[i][0]) - minmin) / (maxmax - minmin);
+
+                int ir = new int();
+                int ig = new int();
+                int ib = new int();
+                ColorScaleBCGYR(thermo_color, ref ir, ref ig, ref ib);
+
+                //MessageBox.Show(ir.ToString() + ig.ToString() + ib.ToString(), "RGB");
+                //MessageBox.Show(i.ToString(), "cyc");
+
+                SolidBrush bbb = new SolidBrush(Color.FromArgb(255, ir, ig, ib));
+
+
+                objGrp.FillRectangle(bbb, new Rectangle(10 * ((i - 2) % 32), 10 * ((i - 2) / 32), 10, 10));
+
+
+                bbb.Dispose();
+
+            }
+            objGrp.Dispose();
+
+        }
+
+        private void 名前をつけて保存SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPeg イメージ|*.jpg|Bitmap イメージ|*.bmp|Png イメージ|*.png";
+            saveFileDialog1.Title = "画像の保存";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+
+                    case 2:
+                        pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+
+                    case 3:
+                        pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                }
+
+                fs.Close();
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (Path.GetExtension(files[0]) == ".csv")
+            {
+                fileName = files[0];
+                var list = loadCSV(fileName);
+                textBox1.Text = list[1][0];
+                DrawThermoImage(list);
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void バージョン情報AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Dialog1 dialog1 = new Dialog1();
+            dialog1.ShowDialog();
+        }
+    }
+    class Dialog1 : Form
+    {
+        Label label;
+        LinkLabel linkLabel;
+        Button okButton;
+        Icon icon;
+        public Dialog1()
+        {
+            // リソースアイコンを取得
+            // (プロジェクト名 Project1，リソース名 Icon の場合)
+            icon = netugazou_v1.Properties.Resources.icon;
+
+            // 48 x 48 の大きさに修正
+            icon = new Icon(icon, 72, 72);
+
+            // アセンブリ情報を取得
+            System.Reflection.AssemblyName assemblyName =
+                System.Reflection.Assembly.GetExecutingAssembly().GetName();
+
+            // プログラムのバージョンを文字列化
+            string productVersion =
+                assemblyName.Version.Major + "." +  // メジャーバージョン
+                assemblyName.Version.Minor + "." +  // マイナーバージョン
+                assemblyName.Version.Build;         // ビルド番号
+
+            string project_name = "Netugazou ";
+            string my_credit = "Copyright (c) 2022 ぬこむーちょ";
+
+            // ラベルの設定
+            label = new Label()
+            {
+                Location = new Point(90, 25),
+                Size = new Size(190, 40),
+                TextAlign = ContentAlignment.TopCenter,
+                Text = project_name + productVersion + Environment.NewLine +
+                    Environment.NewLine + my_credit,
+            };
+
+            string url = "http://www.gunjo-gentokan.com/";
+
+            // リンクラベルの設定
+            linkLabel = new LinkLabel()
+            {
+                Location = new Point(90, 75),
+                Size = new Size(190, 20),
+                TextAlign = ContentAlignment.TopCenter,
+                Text = url,
+            };
+            linkLabel.Links.Add(0, url.Length, url);
+            linkLabel.LinkClicked +=
+                new LinkLabelLinkClickedEventHandler(linkLabel_LinkClicked);
+
+            // ボタンの設定
+            okButton = new Button()
+            {
+                Text = "OK",
+                DialogResult = DialogResult.OK,
+                Location = new Point(100, 110),
+            };
+
+            this.Controls.AddRange(new Control[]
+            {
+            okButton, label, linkLabel,
+            });
+
+            this.ClientSize = new Size(280, 150);
+            this.Text = "バージョン情報";
+
+            // ダイアログボックス用の設定
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.StartPosition = FormStartPosition.CenterParent;
+
+            this.AcceptButton = okButton;
+        }
+
+        void linkLabel_LinkClicked(
+            object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel.LinkVisited = true;
+            System.Diagnostics.Process.Start(e.Link.LinkData as string);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // (25, 25) の位置にアイコンを描画
+            e.Graphics.DrawIcon(icon, 15, 25);
+        }
+    }
+}
